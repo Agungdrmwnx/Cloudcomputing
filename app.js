8a9270e51625b7dfa8222ea24654712809c6c24c
Agungdrmwnx <infinixgaming123@gmail.com>
2024-12-10 01:58:42 -0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Muhammad Agung, 53023008, Bisnis Digital\n')
 })
 
 app.listen(PORT, HOST)
